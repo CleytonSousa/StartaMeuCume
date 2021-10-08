88fa5e082005944d2666b630f4408257e66b2cc1
italoant <italoant@hotmail.com>
2021-10-07 22:01:51 -0300

    base do projeto finalizado, restão as alteracoes

--- pages/card/cardapio.js
+++ pages/card/cardapio.js
@@ -4,6 +4,10 @@ const maiusculo = document.querySelectorAll(".hPratos")
 const pop = document.querySelector(".popUp")
 const fotos = document.querySelectorAll(".contPhoto")
 const fechar = document.querySelector(".closePop")
+const selecionar = document.querySelector(".itensCard")
+const selecionarDois = document.querySelector("#itensCard2")
+const selecionarTreis = document.querySelector("#itensCard3")
+const imgFic = document.getElementById("imagemFic")
 
 
 
@@ -31,17 +35,196 @@ negrito.addEventListener("mouseout", function(event) {
     event.target.style.fontWeight = "";
   }); // retirar mouse no link
 
-fotos.forEach(function(fotos){
-    fotos.addEventListener("click", ()=>{
+fotos.forEach(function(foto){
+    foto.addEventListener("click", ()=>{
         pop.style.display = "block"
-        imagemCerta()
+        conteudo()
 })
 }); //aparecer popUp
 
 fechar.addEventListener("click", () =>{
     pop.style.display = "none"
+}) //fechar popUp
+
+
+selecionar.addEventListener("click", (e) => {
+    conteudo(e.target.id)
 })
 
-const imagemCerta = () =>{
-    
-}
+function conteudo(cont){
+    valor = cont
+    if (valor == "imag0"){
+        imgFic.setAttribute('src','/pages/card/img/comidas/buchada-de-bode-receita-jeito-nordestino.png')
+        document.getElementById('nomeComida').innerHTML = "Buchada de bode"
+        document.getElementById('porcao').innerHTML = "500gm"
+        document.getElementById('acomp1').innerText = "arroz"
+        document.getElementById('acomp2').innerText = "feijao macassar"
+        document.getElementById('acomp3').innerText = "farinha"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$30,00"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if (valor == "imag1"){
+        imgFic.setAttribute('src','/pages/card/img/comidas/sarapatel.png')
+        document.getElementById('nomeComida').innerHTML = "Sarapatel"
+        document.getElementById('porcao').innerHTML = "600gm"
+        document.getElementById('acomp1').innerText = "arroz refogado"
+        document.getElementById('acomp2').innerText = "feijao preto"
+        document.getElementById('acomp3').innerText = "vinagrete"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$40,00"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    }  else if (valor == "imag2"){
+        imgFic.setAttribute('src','/pages/card/img/comidas/galinha-a-cabidela.png')
+        document.getElementById('nomeComida').innerHTML = "Galinha cabidela"
+        document.getElementById('porcao').innerHTML = "300gm"
+        document.getElementById('acomp1').innerText = "arroz refogado"
+        document.getElementById('acomp2').innerText = "feijao tropeiro"
+        document.getElementById('acomp3').innerText = "vinagrete e farofa"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$24,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    }   else if (valor == "imag3"){
+        imgFic.setAttribute('src','/pages/card/img/comidas/maodevaca.png')
+        document.getElementById('nomeComida').innerHTML = "Mão de vaca"
+        document.getElementById('porcao').innerHTML = "400gm"
+        document.getElementById('acomp1').innerText = "arroz branco"
+        document.getElementById('acomp2').innerText = "feijao mulato"
+        document.getElementById('acomp3').innerText = "purê"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$24,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else {
+        null
+    } 
+} //setting da aba de comidas
+
+fotos.forEach(function(fotob){
+    fotob.addEventListener("click", ()=>{
+        conteudoB()
+})
+});
+
+selecionarDois.addEventListener("click", (e) => {
+    conteudoB(e.target.id)
+})
+
+function conteudoB(cont){
+    valorB = cont
+    if (valorB == "imag4"){
+        imgFic.setAttribute('src','/pages/card/img/bebidas/pitu.png')
+        document.getElementById('nomeComida').innerHTML = "Pitu do amor"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Dose"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$5,00"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if(valorB == "imag5"){
+        imgFic.setAttribute('src','/pages/card/img/bebidas/acalcatrao.png')
+        document.getElementById('nomeComida').innerHTML = "Alcatrão puro"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Dose"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$8,00"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if (valorB == "imag6"){
+        imgFic.setAttribute('src','/pages/card/img/bebidas/mojito.png')
+        document.getElementById('nomeComida').innerHTML = "Mojito"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Copo: 350Ml"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$12,00"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if (valorB == "imag7"){
+        imgFic.setAttribute('src','/pages/card/img/bebidas/carreiteiro.png')
+        imgFic.setAttribute('height', '300px')
+        document.getElementById('nomeComida').innerHTML = "Carreteiro"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Copo: 350Ml"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$10,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    }
+} //setting da aba de bebidas
+
+fotos.forEach(function(fotoc){
+    fotoc.addEventListener("click", ()=>{
+        conteudoC()
+})
+});
+
+selecionarTreis.addEventListener("click", (e) => {
+    conteudoC(e.target.id)
+})
+
+function conteudoC(cont){
+    valorC = cont
+    if (valorC == "imag8"){
+        imgFic.setAttribute('src','/pages/card/img/sobremesas/bolo-de-rolo.png')
+        document.getElementById('nomeComida').innerHTML = "Bolo de rolo"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Porção: 300Mg"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$7,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if(valorC == "imag9"){
+        imgFic.setAttribute('src','/pages/card/img/sobremesas/cocada.png')
+        document.getElementById('nomeComida').innerHTML = "Cocada"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Porção: 150Mg"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$4,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if (valorC == "imag10"){
+        imgFic.setAttribute('src','/pages/card/img/sobremesas/munguza.png')
+        document.getElementById('nomeComida').innerHTML = "Munguza"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Porção: 400Mg"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$9,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    } else if (valorC == "imag11"){
+        imgFic.setAttribute('src','/pages/card/img/sobremesas/cartola.png')
+        document.getElementById('nomeComida').innerHTML = "Cartola"
+        document.getElementById('acomp').innerText = ""
+        document.getElementById('acomp1').innerText = ""
+        document.getElementById('acomp2').innerText = ""
+        document.getElementById('acomp3').innerText = ""
+        document.getElementById('porcao').innerHTML = "Porção: 350Mg"
+        var valores = document.getElementById('price')
+        valores.innerText = "valor: R$14,99"
+        valores.setAttribute("id","price")
+        valores.style.fontWeight = "bold"
+    }
+} //setting da aba de Sobremesas
