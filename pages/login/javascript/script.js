b3a2a81175960c3c8c1afce4facd1f2c898273c0
Camila Silva <silva.camilam27@gmail.com>
2021-10-09 00:21:44 -0300

    Adicionando validacoes e ajustando responsividade

--- pages/login/javascript/script.js
+++ pages/login/javascript/script.js
@@ -1,21 +1,39 @@
 
 //validação
 const botaoAcess = document.getElementById("btnAcess");
-botaoAcess.addEventListener("click", function (verificacao){
-    let loginCampo = document.getElementById("login").value;
-    let passCampo = document.getElementById("pass").value;
+botaoAcess.addEventListener("click", function(verificacao){
+    var loginCampo = document.getElementById("login").value;
+    var passCampo = document.getElementById("pass").value;
     
-    if(loginCampo=="" && passCampo==""){
-        document.getElementById("obs").innerHTML = "Campos vazios";
+    if((validarEmail(loginCampo)) && (passCampo.length>=8)){
+        document.getElementById("preencha").style.display = "none";
+        document.getElementById("fundo-popup-boasvindas").style.display = "flex";
         verificacao.preventDefault();
     }
-    
-    //boas-vindas pop-up
+    else{
+        document.getElementById("fundo-popup-boasvindas").style.display = "none";
+        document.getElementById("preencha").style.display = "flex";
+        verificacao.preventDefault();
+    }
 });
 
-// boas-vindas pop-up
-function boasvindas(){
-   
+//Email
+function validarEmail(email){
+    var validacao =  /\S+@\S+.\S+/;
+    return validacao.test(email)
 }
 
-// Esqueceu Senha
+
+console.log("oi")
+
+
+//Esqueceu Senha
+const cliqueAquiLink = document.getElementById("cliqueAqui");
+cliqueAquiLink.addEventListener("click", function(entrarContato){
+    document.getElementById("fundo-popup-senha").style.display="flex"
+});
+//voltar
+const btnVoltar = document.getElementById("btn-voltar");
+btnVoltar.addEventListener("click", function(botaoVoltar){
+    document.getElementById("fundo-popup-senha").style.display="none"
+});
